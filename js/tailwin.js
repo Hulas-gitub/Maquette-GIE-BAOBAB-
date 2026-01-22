3bbeb39b9b59f31d655518fbf45328543c6a8abc
hulas-gitub <sandershulas@gmail.com>
2026-01-22 22:02:01 +0100

    Initial commit

---
+++ js/tailwin.js
@@ -0,0 +1,9 @@
+   tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#195AA2'
+                    }
+                }
+            }
+        }
